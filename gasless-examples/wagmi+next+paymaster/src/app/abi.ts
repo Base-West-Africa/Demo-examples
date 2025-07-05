5af9b5324846ca8966fa3891ab03f8f5e9db2dc3
Mide001 <olamideisreal5@gmail.com>
2025-07-05 02:06:32 +0100

    Add TypeScript ABI for smart contract interactions in Next.js app

---
+++ gasless-examples/wagmi+next+paymaster/src/app/abi.ts
@@ -0,0 +1,40 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+    ],
+    name: "mintTo",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
